fb0f8881f964ae42b34ad58cc2df2393895d6061
mhdi-abo <mhdiabo70@gmail.com>
2025-12-09 22:02:03 +0200

    Update LinkedListArrayALgoritihm.java

--- Second-ALgorithm/LinkedListArrayALgoritihm.java
+++ Second-ALgorithm/LinkedListArrayALgoritihm.java
@@ -6,12 +6,13 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");//format a string to be a date
-        LinkedList[] arr = new LinkedList[10]; // linked lists in array
-        for(int j=0;j<arr.length;j++){
+        LinkedList<Integer>[] arr = new LinkedList[10]; // linked lists in array
+        for(int j=0;j< arr.length;j++){
             arr[j]= new LinkedList<>();
 }
-
         try {
+            int k=0;
+            int conunter=0;
             FileInputStream fis = new FileInputStream("input.txt");
             Scanner sc = new Scanner(fis);
             String line= sc.nextLine();
@@ -20,15 +21,20 @@ public class Main {
             while (sc.hasNextLine()) { //loop from first line to last line in file
                 line = sc.nextLine();
                 line = line.replaceAll("\\s+", "");
-                for (int j = 0; j <10; j++) {
-                    for (int i = 0; i <line.length(); i++) //loop in the line from first char to last char
+
+                    for (int i = 0; i <line.length(); i++){ //loop in the line from first char to last char
                         if (line.charAt(i) == '1') {//make sure its one
-                            arr[j].add(i+1);
+                            arr[k].add(i + 1);
+                            conunter++;
                         }
-                }
+                    }
+                k++;
             }
 
-            System.out.println(arr[0]); //print all lists
+            for (int i =0;i<10;i++){
+            System.out.println(arr[i]); //print all lists
+            }
+            System.out.println(conunter);
 
         }catch (FileNotFoundException e) {
             System.out.println("File not found");
