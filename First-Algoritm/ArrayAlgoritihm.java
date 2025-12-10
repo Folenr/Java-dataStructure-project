d05ccf457e11b33b77cd7ae7febd9ce4a9e9e344
Eyas2006 <iyaschess2006@gmail.com>
2025-12-10 13:15:03 +0200

    Merge ArrayAlgorithm.java by hand

--- First-Algoritm/ArrayAlgoritihm.java
+++ First-Algoritm/ArrayAlgoritihm.java
@@ -11,7 +11,6 @@ public class ArrayAlgoritihm {
         try {
             FileInputStream fis = new FileInputStream("input.txt");
             Scanner sc = new Scanner(fis);
-            Scanner scan = new Scanner(System.in); //scanner for user input
             int counter = 0;
             String line = sc.nextLine(); //first line for the date
             LocalDate Date = LocalDate.parse(line, formatter); //a temp date to turn the String into date
@@ -35,49 +34,28 @@ public class ArrayAlgoritihm {
     }
     //اذا شغال تصلحش
     public static void display(String[] days,int firstDay){
-        Scanner scan = new Scanner(System.in);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
+        Scanner scan = new Scanner(System.in);//scanner for the user input
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");//formating the date
         System.out.print("Enter the Date of format (DD MM YYYY):  ");
         String day = scan.nextLine(); //user enter the day
 
-        LocalDate Date = LocalDate.parse(day, formatter);
-        int currentDay = (int)Date.toEpochDay();
-        int index = currentDay-firstDay; //turn the day into integer
+        LocalDate Date = LocalDate.parse(day, formatter);//convert from string to the format
+        int currentDay = (int)Date.toEpochDay();//convert from date to integer
+        int index = currentDay-firstDay; //get the index of the day in days array
 
         int startMin,endMin;
         System.out.print("Enter the start minutes:  ");
-        startMin = scan.nextInt() - 1;
+        startMin = scan.nextInt() - 1;//get the start min index
         System.out.print("Enter the end minutes:  ");
-        endMin = scan.nextInt() - 1;
+        endMin = scan.nextInt() - 1;//get the end min index
 
-        String hexaNum = ""; //the hexadecimal number we want to print
-        for(int i=startMin/4;i<=endMin/4;i++)
-            hexaNum += days[index].charAt(i);
-
-        char currentHexa = hexaNum.charAt(0); //the current hexadecimal number, it will turn into binary
-        String binary;
-        binary = String.format("%04d", Integer.parseInt(new BigInteger(String.valueOf(currentHexa), 16).toString(2)));
-        //this is 3 parts
-        //1 print the first hexadecimal number (only print the needed part)
-        //2 print the numbers in between
-        //3 print the last hexadecimal number (only print the needed part)
-        if(endMin/4 != startMin/4) //check if we have more than 1 hexadecimal number
-            for(int i=startMin%4;i<4;i++)
-                System.out.print(binary.charAt(i));
-        else{ //if we have 1 hexadecimal number it will print it and stop
-            for(int i=startMin%4;i<=endMin%4;i++)
-                System.out.print(binary.charAt(i));
-            return;
+        String binaryNum = "";
+        //convert every Hexadecimal char to 4 binary chars and add these 4 chars as string to binaryNum
+        for(int i=startMin/4;i<=endMin/4;i++) {
+            binaryNum += String.format("%04d", Integer.parseInt(new BigInteger(String.valueOf(days[index].charAt(i)), 16).toString(2)));
         }
-        for(int i=1;i<hexaNum.length()-1;i++){
-            currentHexa = hexaNum.charAt(i);
-            binary = String.format("%04d", Integer.parseInt(new BigInteger(String.valueOf(currentHexa), 16).toString(2)));
-            for(int j=0;j<4;j++)
-                System.out.print(binary.charAt(j));
+        for(int j=startMin;j<=endMin;j++) {//print them
+            System.out.print(binaryNum.charAt(j));
         }
-        currentHexa = hexaNum.charAt(hexaNum.length()-1);
-        binary = String.format("%04d", Integer.parseInt(new BigInteger(String.valueOf(currentHexa), 16).toString(2)));
-        for(int i=0;i<=endMin%4;i++)
-            System.out.print(binary.charAt(i));
     }
 }
