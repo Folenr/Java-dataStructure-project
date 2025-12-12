776b6a29cd1b5faa740c93b6a8277ce316427892
Eyas2006 <iyaschess2006@gmail.com>
2025-12-13 00:00:46 +0200

    Make the code faster and more Efficient

--- First-Algoritm/ArrayAlgoritihm.java
+++ First-Algoritm/ArrayAlgoritihm.java
@@ -5,36 +5,36 @@ import java.io.*;
 import java.time.*;
 
 public class ArrayAlgoritihm {
-    private int firstDay;
-    private int line;
+    private final int firstDay;
+    private final int line;
     private String[] days;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");
+    private static final Scanner scanner = new Scanner(System.in);
 
     public ArrayAlgoritihm(int line){
         this.line=line;
-        readFile();
+        this.days = new String[this.line];
+        this.firstDay = readFile();
     }
 
-    private void readFile() {
+    private int readFile() {
         java.util.Locale.setDefault(java.util.Locale.US);
-        this.days = new String[line];
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");//format a string to be a date
         try {
             FileInputStream fis = new FileInputStream("input.txt");
             Scanner sc = new Scanner(fis);
+            LocalDate Date = LocalDate.parse(sc.nextLine(), formatter); //a temp date to turn the String into date
             int counter = 0;
-            String line = sc.nextLine(); //first line for the date
-            LocalDate Date = LocalDate.parse(line, formatter); //a temp date to turn the String into date
-            this.firstDay = (int)Date.toEpochDay(); //store the date as Integer : 19 10 2024 -> 20015
-
             while(sc.hasNextLine()) {
-                line = sc.nextLine();
+                String line = sc.nextLine();
                 line = line.replaceAll("\\s+", ""); //delete all spaces
                 this.days[counter] = toHexa(line); //add the result in it specific day
                 counter++;
             }
-
+            sc.close();
+            return (int)Date.toEpochDay(); //store the date as Integer : 19 10 2024 -> 20015
         }catch (FileNotFoundException e) {
             System.out.println("File not found");
+            return 0;
         }
     }
 
@@ -43,24 +43,21 @@ public class ArrayAlgoritihm {
         int index = getIndex();
         int[] range = getRange();
         int startMin = range[0],endMin = range[1];
-
         String binaryNum = toBinary(this.days[index]);
         //take a substring and print it
         System.out.println(binaryNum.substring(startMin, endMin + 1));
     }
 
     public void edit(){
-        Scanner scan = new Scanner(System.in);
         int index = getIndex();
-
         int[] range = getRange();
         int startMin = range[0],endMin = range[1];
         int[] values = new int[endMin-startMin + 1];
         System.out.print("Enter the values: ");
-        String valString = "";
+        StringBuilder valString = new StringBuilder();
         for(int i=0; i<values.length; i++) {
-            values[i] = scan.nextInt();
-            valString += Integer.toString(values[i]);
+            values[i] = Integer.parseInt(scanner.nextLine());
+            valString.append(values[i]);
         }
         String binaryNum = toBinary(this.days[index]);
         //add the value to the zeros and ones
@@ -86,7 +83,7 @@ public class ArrayAlgoritihm {
     }
     public void displayMin(){
         int min =0;
-        for (int i=0;i< this.days.length;i++)
+        for(int i=0;i< this.days.length;i++)
             min += toBinary(this.days[i]).length();
         System.out.println("The total number of minutes is :  " + min);
     }
@@ -112,50 +109,45 @@ public class ArrayAlgoritihm {
 
 
     private int getIndex(){
-        Scanner scan = new Scanner(System.in);//scanner for the user input
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yyyy");//formating the date
         System.out.print("Enter the Date of format (DD MM YYYY):  ");
-        String day = scan.nextLine(); //user enter the day
-
+        String day = scanner.nextLine(); //user enter the day
         LocalDate Date = LocalDate.parse(day, formatter);//convert from string to the format
         int currentDay = (int)Date.toEpochDay();//convert from date to integer
-        int index = currentDay-this.firstDay; //get the index of the day in days array
-        return index;
+        return currentDay-this.firstDay;
     }
 
     private String toHexa(String binary){ //turn a binary to hexa
-        String result = ""; //get the results per day
+        StringBuilder result = new StringBuilder(); //get the results per day
         for(int i = 0; i+4 <= binary.length(); i+=4) {
-            result += Integer.toHexString(Integer.parseInt(binary.substring(i, i+4),2)).toUpperCase();
+            result.append(Integer.toHexString(Integer.parseInt(binary.substring(i, i + 4), 2)).toUpperCase());
         }
         if(binary.length()%4 !=0) {
-            result += "+";
+            result.append("+");
             for(int i=(binary.length()%4);i>0;i--)
-                result += binary.charAt(binary.length()-i);
+                result.append(binary.charAt(binary.length() - i));
         }
-        return result;
+        return result.toString();
     }
     private String toBinary(String hexa){//turn a hexa to binary
-        String binaryNum = "";
+        StringBuilder binaryNum = new StringBuilder();
         for (int i=0; i<hexa.length(); i++) {//convert all hexadecimal char to binary and add all of them to binaryNum
             if(hexa.charAt(i) != '+')
-                binaryNum += String.format("%04d", Integer.parseInt(new BigInteger(String.valueOf(hexa.charAt(i)), 16).toString(2)));
+                binaryNum.append(String.format("%04d", Integer.parseInt(new BigInteger(String.valueOf(hexa.charAt(i)), 16).toString(2))));
             else{
                 hexa = hexa.replaceAll("\\+", "");
                 for (int j=i; j<hexa.length(); j++)
-                    binaryNum += hexa.charAt(j);
+                    binaryNum.append(hexa.charAt(j));
                 break;
             }
         }
-        return binaryNum;
+        return binaryNum.toString();
     }
     private int[] getRange(){
-        Scanner scan = new Scanner(System.in);
         int startMin,endMin;
         System.out.print("Enter the start minute:  ");
-        startMin = scan.nextInt() - 1;
+        startMin = Integer.parseInt(scanner.nextLine()) - 1;
         System.out.print("Enter the end minute:  ");
-        endMin = scan.nextInt() - 1;
+        endMin = Integer.parseInt(scanner.nextLine()) - 1;
         return new int[] {startMin,endMin};
     }
 }
