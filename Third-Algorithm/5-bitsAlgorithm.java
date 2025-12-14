891f2a193fa71363a6d3a258e1a4dee6ab431f40
Folenr <t.a.r252e@gmail.com>
2025-12-14 20:54:20 +0200

    finally fixed the bug

--- Third-Algorithm/5-bitsAlgorithm.java
+++ Third-Algorithm/5-bitsAlgorithm.java
@@ -28,6 +28,7 @@ public class FiveBitsAlgorithm extends SortingMethod{
             }
             this.days = new String[counter];
             sc.close();
+            fis = new FileInputStream("input.txt");
             sc = new Scanner(fis);
             sc.nextLine();//skip the date line
             counter=0;
