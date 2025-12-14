7be9de278ecb0cbf9d3a7b294caf796c016f105a
Folenr <t.a.r252e@gmail.com>
2025-12-14 14:34:30 +0200

    Create SortingMethod.java (the abstract class)

---
+++ SortingMethod.java
@@ -0,0 +1,10 @@
+public abstract  class SortingMethod{
+    abstract void readFile();
+    abstract void search();
+    abstract void edit();
+    abstract void delete();
+    abstract void displayDays();
+    abstract void displayMin();
+    abstract void displayZeros();
+    abstract void displayOnes();
+}
