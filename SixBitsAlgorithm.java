0726c8445daa3098503e63c1671a10321b4e68bb
Eyas2006 <iyaschess2006@gmail.com>
2025-12-15 16:41:13 +0200

    Add size of the file and the compressed size

--- SixBitsAlgorithm.java
+++ SixBitsAlgorithm.java
@@ -44,7 +44,9 @@ public class SixBitsAlgorithm extends SortingMethod{
                     this.days[counter] = to6Bits(line); //add the result in it specific day
                     counter++;
                 }
+                System.out.println("File Size: " + file.length() + " Bytes.");
                 System.out.println("Successfully loaded " + this.days.length + " days of data.");
+                System.out.println("Compressed Size: "+ file.length()/6 + " Bytes.");
             }catch (FileNotFoundException e) {
                 System.out.println("File 'input.txt' not found. Please create data first (option 2 in main menu).");
             }
